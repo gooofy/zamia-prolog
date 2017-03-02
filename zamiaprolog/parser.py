172b90506f51918d9e17f86a699fd0d50512124a
Guenter Bartsch <guenter@zamia.org>
2017-03-02 23:08:02 +0100

    allow extended chars in predicate names

--- zamiaprolog/parser.py
+++ zamiaprolog/parser.py
@@ -79,6 +79,8 @@ NAME_CHARS = set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
                   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
                   '_','0','1','2','3','4','5','6','7','8','9'])
 
+NAME_CHARS_EXTENDED = NAME_CHARS | set([':'])
+
 SIGN_CHARS = set([u'=',u'<',u'>',u'+',u'-',u'*',u'/',u'\\'])
 
 SYM_NONE      =  0
@@ -229,7 +231,7 @@ class PrologParser(object):
             while True:
                 self.cur_str += self.cur_c
                 self.next_c()
-                if not self.cur_c or not (self.cur_c in NAME_CHARS):
+                if not self.cur_c or not (self.cur_c in NAME_CHARS_EXTENDED):
                     break
 
         elif self.cur_c in SIGN_CHARS:
